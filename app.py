94e6d18d049305f70b32b5c2c920f3cc365c48be
Jeremi Alcala <jeremialcala@gmail.com>
2016-11-28 01:09:31 -0400

    nuevo commit

--- app.py
+++ app.py
@@ -46,9 +46,10 @@ def webhook():
                         #msg = "Hola " + j['first_name'] + ", ¿deseas registrarte en TDM?"
                         #msg = "Hola, ¿deseas registrarte en TDM? "
                         msg = sender_id
-                        r = get_user_by_id(recipient_id)
+                        s = get_user_by_id(recipient_id)
+                        log(type(s))
+                        r = get_user_by_id(sender_id)
                         log(type(r))
-
                         #user = get_user_by_id(sender_id)
                         #log(user)
 
